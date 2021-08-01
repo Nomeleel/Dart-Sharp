d4263c67753752bd5a8862a495b0ceed4351777f
Nomeleel <choonlay@outlook.com>
2021-08-01 21:37:37 +0800

    Dart sharp init.

---
+++ src/extension.ts
@@ -0,0 +1,8 @@
+import * as vscode from 'vscode';
+import { DatWrapCodeActionProvider } from './provider/dart_wrap_code_action_provider';
+
+export function activate(context: vscode.ExtensionContext) {
+
+}
+
+export function deactivate() {}
