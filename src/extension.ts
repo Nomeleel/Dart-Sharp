2a4073fa4406a6df016fcca0cd3fc47aeb09e8d7
Nomeleel <choonlay@outlook.com>
2021-10-13 22:25:11 +0800

    Add PubspecViewProvider.

--- src/extension.ts
+++ src/extension.ts
@@ -1,19 +1,23 @@
 import * as vscode from 'vscode';
+import { JumpToEditorCommand } from './command/jump_to_editor_command';
 import { ToggleFormatCommand } from './command/toggle_format_command';
 import { WrapSnippetCommand } from './command/wrap_snippet_command';
 import { DartColorDecoration } from './decoration/dart_color_decoration';
 import { AssetProvider } from './provider/asset_provider';
 import { DatWrapCodeActionProvider } from './provider/dart_wrap_code_action_provider';
+import { PubspecViewProvider } from './provider/pubspec_view_provider';
 
 export function activate(context: vscode.ExtensionContext) {
 
 	/// Command
 	context.subscriptions.push(new ToggleFormatCommand());
 	context.subscriptions.push(new WrapSnippetCommand());
+	context.subscriptions.push(new JumpToEditorCommand());
 
 	/// Provider
 	context.subscriptions.push(new DatWrapCodeActionProvider());
 	context.subscriptions.push(new AssetProvider());
+	context.subscriptions.push(new PubspecViewProvider());
 
   /// Decoration
   context.subscriptions.push(new DartColorDecoration());
