82510195accbfe6df8390710f32b5554231fba71
Nomeleel <choonlay@outlook.com>
2022-08-26 22:45:58 +0800

    Dart sharp add todo for assets.

--- src/provider/asset_provider.ts
+++ src/provider/asset_provider.ts
@@ -95,6 +95,7 @@ export class AssetProvider extends RegExpProvider implements DefinitionProvider,
     let pathDir = pathText.substring(0, splitIndex);
     let searchDir = pathDir ? `${matchAll}${pathDir}${matchAll}` : matchAll;
     let searchPath = `${searchDir}${pathText.substring(splitIndex)}`;
+    // TODO(Nomeleel): 应该只对项目资源文件夹内进行搜索 排除build产物中的资源文件
     return workspace.findFiles(searchPath, 'build');
   }
 
