b4a6fe1b3cc4cbbf06c33bf98301f4b2491ca9a5
Nomeleel <choonlay@outlook.com>
2022-09-24 23:53:58 +0800

    Update search_symbol_command.ts
    Imp detail

--- src/command/search_symbol_command.ts
+++ src/command/search_symbol_command.ts
@@ -1,6 +1,6 @@
-import { commands, InputBoxOptions, SymbolInformation, window } from "vscode";
+import { commands, InputBoxOptions, ProgressLocation, SymbolInformation, window } from "vscode";
 import { DisposableBase } from "../common/disposable_base";
-import { SEARCH_SYMBOL_COMMAND } from "../constant/constant";
+import { JUMP_TO_EDITOR_COMMAND, SEARCH_SYMBOL_COMMAND } from "../constant/constant";
 import { VSCODE_EXECUTE_WORKSPACE_SYMBOL_PROVIDER } from "../constant/vscode";
 
 export class SearchSymbolCommand extends DisposableBase {
@@ -15,18 +15,28 @@ export class SearchSymbolCommand extends DisposableBase {
   static async searchSymbol() {
     let input = await inputSearchSymbol();
     if (input) {
-      const symbolList = await commands.executeCommand<Array<SymbolInformation>>(VSCODE_EXECUTE_WORKSPACE_SYMBOL_PROVIDER, input);
+      const symbolList = await window.withProgress({
+        location: ProgressLocation.Notification,
+        title: 'Search Symbol',
+        cancellable: true,
+      }, () => commands.executeCommand<Array<SymbolInformation>>(VSCODE_EXECUTE_WORKSPACE_SYMBOL_PROVIDER, input));
 
       if (symbolList) {
+        let symbolLocation;
         let symbol = symbolList.find(symbol => symbol.name == input);
-        console.log(symbol);
+        if (symbol) {
+          symbolLocation = symbol.location;
+        } else {
+          let selected = await window.showQuickPick(symbolList.map((symbol) => ({
+            label: symbol.name,
+            description: symbol.containerName,
+            detail: symbol.location.uri.path,
+            location: symbol.location,
+          })));
+          if (selected) symbolLocation = selected.location
+        }
 
-        let selected = await window.showQuickPick(symbolList.map((symbol) => ({
-          label: symbol.name,
-          description: symbol.containerName, 
-          detail: symbol.location.uri.path,
-        })));
-        console.log(selected);
+        if (symbolLocation) commands.executeCommand(JUMP_TO_EDITOR_COMMAND, symbolLocation.uri.path, symbolLocation.range); // TODO: range 不大的话 selected
       }
     }
   }
