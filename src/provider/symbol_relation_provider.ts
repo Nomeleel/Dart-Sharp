fc2a4987a4c855f9f59d138ddd4fb7b6075e487a
Nomeleel <choonlay@outlook.com>
2022-10-30 17:25:08 +0800

    Update symbol_relation_provider.ts
    With progress

--- src/provider/symbol_relation_provider.ts
+++ src/provider/symbol_relation_provider.ts
@@ -1,4 +1,4 @@
-import { Command, commands, Location, Position, TreeItemCollapsibleState, Uri, window, workspace } from "vscode";
+import { CancellationToken, Command, commands, Location, Position, Progress, ProgressLocation, TreeItemCollapsibleState, TreeView, Uri, window, workspace } from "vscode";
 import { TreeDataProviderBase, TreeNode } from "../common/tree_data_provider_base";
 import { SYMBOL_RELATION_COMMAND } from "../constant/constant";
 import { VSCODE_EXECUTE_REFERENCE_PROVIDER } from "../constant/vscode";
@@ -7,20 +7,55 @@ import { getTextFromLocationFillRange } from "../util/util";
 
 export class SymbolRelationProvider extends TreeDataProviderBase<SymbolItem> {
 
+  private treeView: TreeView<TreeNode>;
+
   constructor() {
     super();
     this.disposables.push(
-      window.createTreeView('symbolRelation.view', { treeDataProvider: this, showCollapseAll: true }),
+      this.treeView = window.createTreeView('symbolRelation.view', { treeDataProvider: this, showCollapseAll: true }),
       commands.registerCommand(SYMBOL_RELATION_COMMAND, this.symbolRelation, this),
     );
   }
 
   public async symbolRelation() {
-    this.updateTreeView(await symbolRelationScan());
+    const symbols = await window.withProgress({
+      location: ProgressLocation.Notification,
+      // title: 'Scan Symbol Relation...',
+      cancellable: true,
+    }, this.symbolRelationScanWithProgress);
+
+    if (symbols) {
+      this.updateTreeView(symbols);
+      this.treeView.reveal(this.rootNode!);
+    }
   }
+
+  private symbolRelationScanWithProgress(progress: Progress<{ message?: string | undefined; increment?: number | undefined; }>, token: CancellationToken): Promise<SymbolItem | undefined> {
+    let progressMap = new Map<string, number>();
+    function progressTrack(key: string, preKey: string, count: number, message: string): boolean {
+      if (token.isCancellationRequested) return false;
+      let increment = 0;
+      if (progressMap.has(key)) {
+        increment = progressMap.get(key)!;
+      } else {
+        increment = (progressMap.get(preKey) ?? 100) / ((count ?? 0) + 1);
+        progressMap.set(key, increment);
+      }
+
+      progress.report({
+        message: message,
+        increment: increment,
+      });
+
+      return true;
+    }
+
+    return symbolRelationScan(progressTrack);
+  }
+
 }
 
-async function symbolRelationScan(uri?: Uri, position?: Position, symbolItem?: SymbolItem): Promise<SymbolItem | undefined> {
+async function symbolRelationScan(progressTrack: Function, uri?: Uri, position?: Position, symbolItem?: SymbolItem): Promise<SymbolItem | undefined> {
   let curUri = uri ?? window.activeTextEditor?.document.uri;
   let curPosition = position ?? window.activeTextEditor?.selection.start;
   if (!curUri || !curPosition) return;
@@ -30,6 +65,8 @@ async function symbolRelationScan(uri?: Uri, position?: Position, symbolItem?: S
   if (symbolItem == null) symbolItem = new SymbolItem(symbol, uri);
   symbolItem.setCommand(jumpToCommand(curUri, curRange));
   let locations = await commands.executeCommand<Array<Location>>(VSCODE_EXECUTE_REFERENCE_PROVIDER, curUri, curPosition);
+  let track = progressTrack(symbol, symbolItem?.parent?.label, locations?.length, symbol);
+  if (!track) return;
   if (locations) {
     for await (const location of locations) {
       let rangeText = await getTextFromLocationFillRange(location);
@@ -39,7 +76,10 @@ async function symbolRelationScan(uri?: Uri, position?: Position, symbolItem?: S
         symbolItem.setExpanded();
         let curSymbolItem = new SymbolItem(match.groups!.symbol, location.uri, symbolItem);
         symbolItem.children.push(curSymbolItem);
-        await symbolRelationScan(location.uri, new Position(location.range.start.line, match.index + 1), curSymbolItem);
+        await symbolRelationScan(progressTrack, location.uri, new Position(location.range.start.line, match.index + 1), curSymbolItem);
+      } else {
+        track = progressTrack(symbol, symbolItem?.parent?.label, locations?.length, rangeText);
+        if (!track) return;
       }
     }
   }
@@ -52,7 +92,7 @@ export class SymbolItem extends TreeNode {
     this.resourceUri = uri;
     this.parent = parent;
   }
-  
+
   public setCommand(command?: Command) {
     this.command = command;
   }
