e7e4dec7527bc095ed322552ab6d13b4024daf06
Choonlay Lee <choonlay@outlook.com>
2023-08-18 09:37:17 +0000

    Refine doc comment reg rxp.

--- src/command/copy_commentary_command.ts
+++ src/command/copy_commentary_command.ts
@@ -3,7 +3,7 @@ import { DisposableBase } from "../common/disposable_base";
 import { COPY_COMMENTARY_COMMAND } from "../constant/constant";
 import { getRangeText } from "../util/document";
 
-export const documentCommentPrefixSlash = '\n/// ';
+export const documentCommentPrefixSlash = '\n? */// ?';
 
 export class CopyCommentaryCommand extends DisposableBase {
   constructor() {
@@ -23,5 +23,5 @@ function copyToClipboard(document?: TextDocument, range?: Range, shrink = true) 
 }
 
 export function getRangeCommentaryText(range?: Range, shrink = true): string | undefined {
-  return getRangeText(range)?.replace(RegExp(documentCommentPrefixSlash, 'gmi'), shrink ? '' : '\n');
+  return getRangeText(range)?.replace(RegExp(documentCommentPrefixSlash, 'gmi'), shrink ? ' ' : '\n');
 }
